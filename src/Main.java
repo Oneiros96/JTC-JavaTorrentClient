047c3099be9ba5f1397afe02a61ad5a80c0df938
Oneiros96 <oneiros9692@gmail.com>
2024-06-25 11:17:37 +0200

    todo list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //todo:
+            //take torrent file as input
+            //bencode parser to get information from torrent file
+            //connet to tracker server, get peers
+            //peer-to-peer communication
+            //download/verify pieces -> assemble files
+            //upload peaces
 
+        //
 
     }
 }
